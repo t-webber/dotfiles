787082756d7b74e3fbd79bca211128dff69649c2
Tom Webber <t.c.w.webber@gmail.com>
2025-05-30 23:08:59 +0200

    (settings)[providers] rm: useless python provider

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -12,7 +12,7 @@ local g = vim.g
 g.c_syntax_for_h = 1 -- consider .h files as .c files
 g.loaded_node_provider = 0
 g.loaded_perl_provider = 0
--- g.loaded_python3_provider = 0
+g.loaded_python3_provider = 0
 g.loaded_ruby_provider = 0
 
 -- show inline errors
