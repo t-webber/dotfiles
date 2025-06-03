002d60bb5217cec554681985db86a76fda50bb42
Tom Webber <t.c.w.webber@gmail.com>
2025-06-03 09:00:02 +0200

    (etc) rm: resolv.conf

--- etc/link.sh
+++ etc/link.sh
@@ -1,13 +1,13 @@
-#!/bin/sh 
+#!/bin/sh
 
-if [[ -z $ETC ]]; then 
+if [[ -z $ETC ]]; then
 	echo "ETC var not configured"
 	exit 1
 fi
 
 link() {
 	path="$1"
-	if [[ -z $2 ]]; then 
+	if [[ -z $2 ]]; then
 		file="$(echo $path | awk -F/ '{print $NF}')"
 	else
 		file="$2"
@@ -27,11 +27,9 @@ link "/etc/locale.gen"
 link "/etc/locale.conf"
 link "/etc/inputrc"
 
-link "/etc/resolv.conf"
-link "/etc/iwd" 
+link "/etc/iwd"
 
 link "/etc/pacman.conf"
-link "/etc/pacman.d/mirrorlist" 
+link "/etc/pacman.d/mirrorlist"
 
-link "/etc/X11/xorg.conf.d/50-libinput.conf" 
-
+link "/etc/X11/xorg.conf.d/50-libinput.conf"
