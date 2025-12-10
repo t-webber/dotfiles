603386d2b59d36afe4eb97fa6900615396697ee0
Tom Webber <t.c.w.webber@gmail.com>
2025-12-10 08:45:40 +0100

    fix rn exit code on failure

--- cmd/src/rn.c
+++ cmd/src/rn.c
@@ -10,74 +10,33 @@
 #include <time.h>
 #include <unistd.h>
 
-static uint64_t since_unix(void) {
+const uint64_t NS = 1;
+const uint64_t SEC = 1000 * 1000 * 1000 * NS;
+
+mustuse static uint64_t since_unix(void) {
         struct timespec ts;
         clock_gettime(CLOCK_REALTIME, &ts);
         const uint64_t nsec = (uint64_t)ts.tv_nsec;
         const uint64_t sec = (uint64_t)ts.tv_sec;
-        return (nsec + sec * 1000 * 1000 * 1000);
+        return (nsec * NS + sec * SEC);
 }
 
-static void rn_file(const_str filename) {
+noreturn nonnull static void rn_file(const_str filename) {
         const_str waste = getenv_checked("WASTE");
         mkdir(waste, 0755);
 
         char path[256];
-        snprintf(path, sizeof path, "%s/%" PRIu64 "/", waste, since_unix());
+        snprintf(path, sizeof(path), "%s/%" PRIu64 "/", waste, since_unix());
         printf("%s => %s\n", filename, path);
         mkdir(path, 0755);
 
         exldn("mv", filename, path);
 }
 
-static uint64_t unit_amount(const char unit) {
-        const uint64_t sec = 1e9;
-
-        switch (unit) {
-
-        case 'h':
-                return 3600 * sec;
-
-        case 'd':
-                return 24 * 3600 * sec;
-
-        default:
-                upanic("invalid unit, use h or d")
-        }
-}
-
-static void clean_waste(const uint64_t amount) {
-        const uint64_t boundary = since_unix() - amount;
-        printf("Deleting under %" PRIu64, boundary);
-
-        DIR *bin = opendir_checked(getenv_checked("WASTE"));
-        const struct dirent *file;
-
-        while ((file = readdir(bin))) {
-                if (file->d_name[0] == '.') continue;
-                const size_t date = (size_t)atoi(file->d_name);
-                if (date < boundary) printf("removing %s\n", file->d_name);
-        }
-}
-
 int main(int argc, Args argv) {
         store_usage(argv);
-        const bool clear = is_verbose(argv[0], "rn", "rnc");
-
-        if (clear) {
-                if (argc == 2 && !strcmp(argv[1], "all")) {
-                        chdir(getenv_checked("WASTE"));
-                        system("fd --hidden --ignore | while read p; sleep "
-                               "0.01; sudo rm -rf $p & done");
-                        exit(0);
-                }
-                if (argc != 3) upanic("Usage: rnc <all|<h|j|s|m> <amout>>\n");
-                clean_waste(unit_amount(*argv[1])
-                            * strtoull(argv[2], NULL, 10));
-                return 0;
-        }
-
         if (argc == 1) upanic("Missing arguments...");
+        bool all_ok = false;
 
         pid_t *pids = malloc(sizeof(pid_t) * (size_t)(argc - 1));
 
@@ -86,9 +45,10 @@ int main(int argc, Args argv) {
                 pids[i] = pid;
                 ++i;
                 if (pid == 0) { rn_file(argv[i]); }
+                all_ok = all_ok && (access(argv[i], F_OK));
         }
 
         for (int i = 0; i < argc - 1; ++i) { fork_wait(pids[i]); }
 
-        return 0;
+        return all_ok ? 0 : 1;
 }
