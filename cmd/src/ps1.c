979585ec5365949bc9a1e7d0e9143246604bf2ef
Tom Webber <twebber@tesla.com>
2026-02-24 10:25:57 +0100

    tm

--- cmd/src/ps1.c
+++ cmd/src/ps1.c
@@ -99,8 +99,6 @@ static char *get_git_branch(void) {
         return branch;
 }
 
-#define COL(x) "\001\x1b[" #x "m\002"
-
 static void battery_warnings(const battery_status status, int battery) {
         const bool can_use_dunst = getenv("NO_DUNST") == NULL;
 
@@ -124,10 +122,10 @@ static void battery_warnings(const battery_status status, int battery) {
 __wur static char get_battery(const char **const colour) {
         const battery_status status = get_battery_status();
 
-        *colour = status == BATTERY_STATUS_FULL          ? COL(35)
-                  : status == BATTERY_STATUS_DISCHARGING ? COL(31)
-                  : status == BATTERY_STATUS_CHARGING    ? COL(32)
-                                                         : COL(33);
+        *colour = status == BATTERY_STATUS_FULL          ? MAGENTA
+                  : status == BATTERY_STATUS_DISCHARGING ? RED
+                  : status == BATTERY_STATUS_CHARGING    ? GREEN
+                                                         : YELLOW;
 
         const char *battery = get_battery_level();
         if (battery == NULL) battery = "??";
@@ -158,7 +156,13 @@ int main(void) {
 
         char *const branch = get_git_branch();
 
-        printf(COL(35) "%s%s%c" COL(33) "%x%d" COL(36) "%s" COL(32) "%s" COL(0),
+        const_str venv = getenv("VIRTUAL_ENV") ? "v" : "";
+        const_str root = !strcmp(getenv_checked("USER"), "root") ? "#" : "";
+
+        printf(BLUE "%s%s" MAGENTA "%s%s%c" YELLOW "%x%d" CYAN "%s" GREEN
+                    "%s" RESET,
+               venv,
+               root,
                dev,
                bat_col,
                bat,
